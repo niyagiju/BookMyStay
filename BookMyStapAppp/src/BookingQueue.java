7857b8b63bbf8cfe2dd0f656d545eb1bff544f68
niyagiju <gijuniya@gmail.com>
2026-03-17 09:50:08 +0530

    Bookinh Queue

--- BookMyStapAppp/src/BookingQueue.java
+++ BookMyStapAppp/src/BookingQueue.java
@@ -1,28 +1,18 @@
 import java.util.*;
 
-/**
- * Handles booking requests using FIFO Queue
- */
 public class BookingQueue{
 
     private Queue<Reservation> queue=new LinkedList<>();
 
-    // Add request
     public void addRequest(Reservation r){
         queue.add(r);
-        System.out.println("Request added for "+r.getGuestName());
     }
 
-    // View all requests
-    public void displayQueue(){
-        System.out.println("\n===== Booking Queue =====");
-        for(Reservation r:queue){
-            r.display();
-        }
+    public Reservation getNext(){
+        return queue.poll(); // FIFO
     }
 
-    // Get next request (FIFO)
-    public Reservation getNextRequest(){
-        return queue.poll();
+    public boolean isEmpty(){
+        return queue.isEmpty();
     }
 }
