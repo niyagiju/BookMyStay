8389ad0fe0e4c44aea8c90e0f7c16beef828d0a7
niyagiju <gijuniya@gmail.com>
2026-03-17 09:36:36 +0530

    Reservation class

---
+++ BookMyStapAppp/src/Reservation.java
@@ -0,0 +1,25 @@
+/**
+ * Represents a booking request made by a guest
+ */
+public class Reservation{
+
+    private String guestName;
+    private String roomType;
+
+    public Reservation(String guestName,String roomType){
+        this.guestName=guestName;
+        this.roomType=roomType;
+    }
+
+    public String getGuestName(){
+        return guestName;
+    }
+
+    public String getRoomType(){
+        return roomType;
+    }
+
+    public void display(){
+        System.out.println("Guest: "+guestName+" | Requested: "+roomType);
+    }
+}
